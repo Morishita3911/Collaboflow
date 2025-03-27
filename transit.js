c462c3c73500146881e9e3900aeebc792c602cc9
森下 信也 <morishitas@Havix-Network.local>
2025-03-28 08:53:23 +0900

    Update all files

--- transit.js
+++ transit.js
@@ -3,5 +3,6 @@
     // 新規申請画面を開くとメッセージを表示
     collaboflow.events.on('request.input.show', function (data) {
         alert("welcome! コラボフローJavaScript API!");
+        alert("welcome! コラボフローJavaScript API!");
     });
 })();
