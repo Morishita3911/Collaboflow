54a4fd64921c8476ca83d5a3046a01f113921a66
森下 信也 <morishitas@Havix-Network.local>
2025-04-08 17:33:39 +0900

    Update all files

--- test.js
+++ test.js
@@ -1,13 +1,14 @@
 (function () {
-    'use strict';
-    // フォームの送信ボタンが押されたときの処理
-    Collaboflow.events.on('request.input.check', function (data) {
-        const parts = data.parts;
 
-        const tableData = parts['fid22'].value;
+    'use strict';
+
+    // フォームの送信ボタンが押されたときの処理
+    Collaboflow.events.on(['request.input.show'], function (data) {
+        //const parts = data.parts;
+
+        //const tableData = parts['fid22'].value;
         
-        alert(tableData);
-        console.log(tableData);
-        exit;
+        //alert(tableData);
+        console.log(111);
     });
 })();
