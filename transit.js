75a074f5b2c750a091066369bdc40ba2eea2801d
森下 信也 <morishitas@Havix-Network.local>
2025-04-08 16:48:17 +0900

    Update all files

--- transit.js
+++ transit.js
@@ -1,11 +1,24 @@
 (function () {
+    'use strict';
     // フォームの送信ボタンが押されたときの処理
-    Collaboflow.events.on('request.input.check', function (event) {
+    Collaboflow.events.on('request.input.check', function (data) {
+        const parts = data.parts;
+
+        const tableData = parts['tbl_1'].value;
         var errorFlag = false; // エラー判定フラグ
         var fieldIds = ['fid21', 'fid29', 'fid30', 'fid31']; // 明細のパーツID
         var hasValue = false; // どれか1つに値があるか
-
+        alert(tableData);
         // 明細をループでチェック
+        tableData.forEach(function (row) {
+            // 各行のフィールドをチェック
+            fieldIds.forEach(function (fieldId) {
+                var fieldValue = row[fieldId]; // フィールドの値を取得
+                if (fieldValue && fieldValue.trim() !== '') {
+                    hasValue = true; // どれか1つでも値が入っていれば判定
+                }
+            });
+        });
         for (var i = 0; i < fieldIds.length; i++) {
             var field = Collaboflow.getFieldValue(fieldIds[i]); // フィールドの値を取得
             if (field && field.trim() !== '') {
